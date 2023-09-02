125ff361fad4fe7507945106d4f0e21f2cbb9007
M1K259 <87774553+Mika259@users.noreply.github.com>
2023-09-02 21:30:45 +0800

    Update ytdw.py

--- ytdw.py
+++ ytdw.py
@@ -1,14 +1,26 @@
-from pytube import YouTube
+try:
+    from pytube import YouTube
+except:
+    print("try install 'pytube' command : pip install pytube ")
+    exit()
 print("========================")
 print("Muat Turun Video YouTube")
 #Minta link dari user
 link = input("Masukkan Link :")
 #Manipulasi link
-yt = YouTube(link)
-
+try:
+   yt = YouTube(link)
+   user = input("Muat turun resolusi paling tinggi?(y/N)")
+except:
+   exit(f"Link yang dimasukkan bermasalah, sila cuba lagi.")
 #resolusi paling tinggi
-rs = yt.streams.get_highest_resolution()
-print("Muat Turun Video..")
-rs.download()
-print("Berjaya Muat Turun Video")
+def dw():
+    print("sedang memuat turun video !...")
+    rs = yt.streams.get_highest_resolution()
+    rs.download()
+    print("Berjaya Muat Turun Video ✅")
 print("========================")
+if user == "y":
+    dw()
+else:
+    exit()
